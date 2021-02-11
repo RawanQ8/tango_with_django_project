acf239d91d74543e4eba0721c1cf7a37d07ea0c8
Rawan Jeraq <2478575j@student.gla.ac.uk>
2021-02-11 11:45:20 +0300

    chapter8p1

--- populate_rango.py
+++ populate_rango.py
@@ -8,38 +8,47 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
     {'title':'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/',
+        'views': 42},
     {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/',
+        'views': 23},
     {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'}
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views':69}
     ]
 
     django_pages = [
     {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views':70},
     {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         'views':120},
     {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'}
+         'url':'http://www.tangowithdjango.com/',
+         'views':24}
     ]
 
     other_pages = [
     {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views': 3},
     {'title':'Flask',
-         'url':'http://flask.pocoo.org'}
+         'url':'http://flask.pocoo.org',
+         'views': 4}
     ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64},
+            'Django': {'pages': django_pages, 'views':64, 'likes':32},
+            'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16}}
 
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'],cat_data['likes'])
+
         for p in cat_data['pages']:
-            add_page(c,p['title'],p['url'])
+            add_page(c,p['title'],p['url'],p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -54,8 +63,10 @@ def add_page(cat,title,url,views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c= Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save
     return c
 
--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category (models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name_max_length = 128
+    name = models.CharField(max_length=name_max_length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    
-    class Meta():
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category,self).save(*args, **kwargs)
+
+    class Meta:
         verbose_name_plural = 'Categories'
 
     def __str__(self):
@@ -14,8 +21,10 @@ class Category (models.Model):
 
 
 class Page(models.Model):
+    title_max_length = 128
+    url_max_length = 200
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=title_max_length)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
