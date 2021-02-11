88139e6a975d2ea226b36bfb74014d7d299350b2
Rawan Jeraq <2478575j@student.gla.ac.uk>
2021-02-11 16:09:55 +0300

    chapter8p2

--- rango/views.py
+++ rango/views.py
@@ -22,6 +22,7 @@ def index(request):
 
 def about(request):
     context_dict = {'your_name':'Rawan'}
+
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request,category_name_slug):
@@ -49,7 +50,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
 
@@ -65,7 +66,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
 
     form = PageForm()
