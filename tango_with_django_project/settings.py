b9df8cdf62cc71c23ba91c528e09c58706b0bcf4
Rawan Jeraq <2478575j@student.gla.ac.uk>
2021-02-12 22:43:55 +0300

    chapter9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -94,6 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'Options': {'min_length':6}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -102,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGIN_URL = 'rango:login'
 
 
 # Internationalization
